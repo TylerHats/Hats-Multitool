cd28d9d674ea71d0b8a00d2a252b9268723955b3
Tyler Hatfield <tyler@hatsthings.com>
2025-04-26 18:47:59 -0400

    Containerized Setup Script

--- Core.ps1
+++ Core.ps1
@@ -50,82 +50,18 @@ Write-Host ""
 Hide-ConsoleWindow
 $MainMenu.ShowDialog() | Out-null
 
-# Display Module Selection GUI
+# Run PC Setup/Config GUI and Script
 if ($Show_SetupGUI) {
 	$ModGUI.ShowDialog() | Out-null
 	Show-ConsoleWindow
-}
-
-# Run Time Zone Module
-if ($Run_TimeZoneSetting) {
-	$TZPath = Join-Path -Path $PSScriptRoot -ChildPath 'TimeZone.ps1'
-	. "$TZPath"
-	Write-Host ""
-}
-
-# Setup prerequisites and start Windows update module
-if ($Run_WindowsUpdates) {
-	$WindowsUpdateModPath = Join-Path -Path $PSScriptRoot -ChildPath 'WindowsUpdate.ps1'
-	Log-Message "Install Cumulative updates for Windows? (These can be very slow) (y/N):" "Prompt"
-	$env:installCumulativeWU = Read-Host
-	Log-Message "Starting Windows Updates in the Background..."
-	$ProgressPreference = 'SilentlyContinue'
-	Install-PackageProvider -Name NuGet -Force | Out-File -Append -FilePath $logPath
-	Install-Module -Name PSWindowsUpdate -Force | Out-File -Append -FilePath $logPath
-	Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass", "-File `"$WindowsUpdateModPath`""
-	Write-Host ""
-}
-
-# Run accounts module
-if ($Run_LocalAccountSetup) {
-	$AccountsModPath = Join-Path -Path $PSScriptRoot -ChildPath 'Accounts.ps1'
-	. "$AccountsModPath"
-	Write-Host ""
-}
-
-# Run WinGet setup module
-if ($Run_ProgramInstallation) {
-	$WinGetSetupModPath = Join-Path -Path $PSScriptRoot -ChildPath 'WinGetSetup.ps1'
-	. "$WinGetSetupModPath"
-	Write-Host ""
-}
-
-# Run bloat cleanup module
-if ($Run_BloatCleanup) {
-	$BloatCleanupModPath = Join-Path -Path $PSScriptRoot -ChildPath 'BloatCleanup.ps1'
-	. "$BloatCleanupModPath"
-	Write-Host ""
-}
-
-# Run program installation module
-if ($Run_ProgramInstallation) {
-	$ProgramsModPath = Join-Path -Path $PSScriptRoot -ChildPath 'Programs.ps1'
-	. "$ProgramsModPath"
-	Write-Host ""
-}
-
-# Run system management module
-if ($Run_SystemManagement) {
-	$SystemManagementModPath = Join-Path -Path $PSScriptRoot -ChildPath 'SystemManagement.ps1'
-	. "$SystemManagementModPath"
-	Write-Host ""
-}
-
-# Final setup options
-$regPathNumLock = "Registry::HKEY_USERS\.DEFAULT\Control Panel\Keyboard"
-if (Test-Path $regPathNumLock) {
-    # Set the InitialKeyboardIndicators value to 2 (Enables numlock by default) and disable Fast Startup for registry loading
-    Set-ItemProperty -Path $regPathNumLock -Name "InitialKeyboardIndicators" -Value "2"
-	powercfg /hibernate off *>&1 | Out-File -Append -FilePath $logPath
-    Log-Message "Enabled NUM Lock at boot by default." "Success"
-} else {
-    Log-Message "Registry path $regPathNumLock does not exist." "Error"
+	$SetupScriptModPath = Join-Path -Path $PSScriptRoot -ChildPath 'SetupScript.ps1'
+	. "$SetupScriptModPath"
 }
 
 # Reminders/Closing
-Log-Message "Setup is complete!"
-Log-Message "Confirm updates have completed in the minimized window and restart to apply updates, PC name change and domain/EntraID joining if needed."
-Log-Message "Press enter to exit the script." "Success"
+Log-Message "The multitool run has completed!"
+Log-Message "Verify no other windows are still working on background tasks after closing, then reboot if needed to complete setup, updates, etc."
+Log-Message "Press enter to exit the core script and run self-cleanup." "Success"
 Read-Host
 
 # Post execution cleanup
