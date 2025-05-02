f199c25d4cf884faf3652f741d6ed8d9b078e8f8
Tyler Hatfield <tyler@hatsthings.com>
2025-05-02 16:52:58 -0400

    Improved Version Checking Code

--- Update.ps1
+++ Update.ps1
@@ -1,7 +1,8 @@
-# Self Update Module - Tyler Hatfield - v1.4
+# Self Update Module - Tyler Hatfield - v1.5
 
 # Check program version against remote, update if needed
-$currentVersion = "2.1.1"
+$currentVersionString = "2.1.1"
+[version]$currentVersion = $currentVersionString
 $skipUpdate = 0
 Try {
 	$remoteRequest = Invoke-WebRequest -Uri "https://hatsthings.com/HatsScriptsVersion.txt"
@@ -11,13 +12,38 @@ Try {
 	$skipUpdate = 1
 }
 if ($skipUpdate -ne 1) {
-	$remoteVersion = $remoteRequest.Content
+	$remoteVersionString = $remoteRequest.Content
+	[version]$remoteVersion = $remoteVersionString
 	if ($currentVersion -eq $remoteVersion) {
 		if ($env:hatsUpdated -eq "1") {
 			Log-Message "Program updated successfully! (Version $currentVersion)" "Success"
 		} else {
-			Log-Message "The script is up to date. (Version $currentVersion)" "Info"
+			Log-Message "The Hat's Multitool is up to date. (Version $currentVersion)" "Info"
 			Write-Host ""
+		}
+	} elseif ($currentVersion -gt $remoteVersion) {
+		Log-Message "The program is newer than the remote version. Download from the web and relaunch? (y/N)" "Prompt"
+		$ReplaceNewer = Read-Host
+		if ($ReplaceNewer -match 'y|yes') {
+			Log-Message "Downloading and relaunching the script... (Current Version: $currentVersion - Remote Version: $remoteVersion)" "Info"
+			$sourceURL = "https://github.com/TylerHats/Hats-Multitool/releases/latest/download/Hats-Multitool-v$remoteVersion.exe"
+			$shell = New-Object -ComObject Shell.Application
+			$downloadsFolder = $shell.Namespace('shell:Downloads').Self.Path
+			$outputPath = "$downloadsFolder\Hats-Multitool-v$remoteVersion.exe"
+			Try {
+				Invoke-WebRequest -Uri $sourceURL -OutFile $outputPath *>&1
+			} catch {
+				Log-Message "Failed to download update, please update manually." "Error"
+				Pause
+				$ForceExit = $true
+			}
+			# Cleanup and exit current script, then launch updated script
+			$folderToDelete = "$PSScriptRoot"
+			$deletionCommand = "Start-Sleep -Seconds 2; Remove-Item -Path '$folderToDelete' -Recurse -Force; Add-Content -Path '$logPath' -Value 'Script self cleanup completed during self update'; Start-Process '$outputPath'"
+			Start-Process powershell.exe -ArgumentList "-NoProfile", "-WindowStyle", "Hidden", "-Command", $deletionCommand
+			$ForceExit = $true
+		} else {
+			Log-Message "Proceed with caution, and if you run into errors please redownload from the web." "Skip"
 		}
 	} else {
 		Log-Message "Updating and relaunching the script... (Current Version: $currentVersion - Remote Version: $remoteVersion)" "Info"
