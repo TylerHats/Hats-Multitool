6631d5d2c7df127ff2f197d5c5538ca22ffb94ac
Tyler Hatfield <tyler@hatsthings.com>
2025-05-21 13:35:38 -0400

    Added Show-Console Window Call After Starting Updates

--- SetupScript.ps1
+++ SetupScript.ps1
@@ -22,6 +22,7 @@ if ($Run_WindowsUpdates) {
 	Install-Module -Name PSWindowsUpdate -Force | Out-File -Append -FilePath $logPath
 	Start-Process -FilePath "powershell.exe" -ArgumentList "-ExecutionPolicy Bypass", "-File `"$WindowsUpdateModPath`""
 	Write-Host ""
+	Show-ConsoleWindow
 }
 
 # Run accounts module
