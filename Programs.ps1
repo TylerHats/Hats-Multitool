976520fe01be2292c68d0ea9c0f812b6c1bceebd
Tyler Hatfield <tyler@hatsthings.com>
2025-05-21 13:59:28 -0400

    Adjusted New Progressbar Positioning

--- Programs.ps1
+++ Programs.ps1
@@ -100,9 +100,10 @@ $officeCheckbox.Add_CheckedChanged({
 })
 
 # Container panel with border
+$y += 10
 $trackPanel = New-Object System.Windows.Forms.Panel
-$trackPanel.Size        = [System.Drawing.Size]::new(462,22)
-$trackPanel.Location    = [System.Drawing.Point]::new(14,19)
+$trackPanel.Size        = [System.Drawing.Size]::new(360,22)
+$trackPanel.Location    = [System.Drawing.Point]::new(20,$y)
 $trackPanel.BorderStyle = 'FixedSingle'
 $trackPanel.BackColor   = [System.Drawing.Color]::DarkGray
 $form.Controls.Add($trackPanel)
