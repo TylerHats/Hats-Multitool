0bbe11b4482848fb4ea03460d635979a152b5e1e
Tyler Hatfield <tyler@hatsthings.com>
2026-06-18 15:01:34 -0400

    Added Cycling Dot Animation

--- BGReminder.ps1
+++ BGReminder.ps1
@@ -1,4 +1,4 @@
-# Background Reminder Module - Tyler Hatfield - v1.6
+# Background Reminder Module - Tyler Hatfield - v1.7
 
 # Prepare form
 $BGR = New-Object System.Windows.Forms.Form
@@ -14,9 +14,9 @@ $BGR.AutoScaleDimensions = New-Object System.Drawing.SizeF(96, 96)
 $BGR.AutoScaleMode = [System.Windows.Forms.AutoScaleMode]::Dpi
 Set-DarkTitleBar -TargetForm $BGR
 
-# Add descriptive label
+# Add descriptive label (Removed the dots from the base string)
 $BGRlabel = New-Object System.Windows.Forms.Label
-$BGRlabel.Text = "Hat's Multitool is running..."
+$BGRlabel.Text = "Hat's Multitool is running"
 $BGRlabel.ForeColor = [System.Drawing.ColorTranslator]::FromHtml("#d9d9d9")
 $BGRlabel.Dock = [System.Windows.Forms.DockStyle]::Fill
 $BGRlabel.TextAlign = 'MiddleCenter'
@@ -24,12 +24,25 @@ $BGR.Controls.Add($BGRlabel)
 
 # Define a function to handle window closing
 $BGR.Add_FormClosed({
-	param($sender, $e)
-	if ($e.CloseReason -eq [System.Windows.Forms.CloseReason]::UserClosing -and (-not $BGRCodeExit)) {
-		Log-Message "User exited, running cleanup."
-		User-Exit
-	}
+    param($sender, $e)
+    if ($e.CloseReason -eq [System.Windows.Forms.CloseReason]::UserClosing -and (-not $BGRCodeExit)) {
+        Log-Message "User exited, running cleanup."
+        User-Exit
+    }
 })
 
-# Display GUI
+$global:bgrDotCount = 0
+$BGRTimer = New-Object System.Windows.Forms.Timer
+$BGRTimer.Interval = 1000 # Updates every 1000 milliseconds (a second)
+$BGRTimer.Add_Tick({
+    $global:bgrDotCount++
+    if ($global:bgrDotCount -gt 3) { $global:bgrDotCount = 0 }
+    
+    # Multiply the dot character by the count to create the trail
+    $dots = "." * $global:bgrDotCount 
+    $BGRlabel.Text = "Hat's Multitool is running$dots"
+})
+
+# Start the timer and Display GUI
+$BGRTimer.Start()
 $BGR.Show() | Out-Null
