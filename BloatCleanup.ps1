c3655419f5f85e69d1b147d19bd76d287e1ec6b9
Tyler Hatfield <tyler@hatsthings.com>
2026-06-23 11:45:04 -0400

    Adjusted whitespace

--- BloatCleanup.ps1
+++ BloatCleanup.ps1
@@ -2,7 +2,7 @@
 
 # $RemoveBloat = "y"
 
-<# 
+<#
 List of common Windows 11 OEM and Consumer bloatware.
 Wildcards (*) are used to catch varying version names.
 #>
@@ -29,17 +29,17 @@ $removedCount = 0
 
 foreach ($app in $bloatApps) {
     Log-Message "Attempting to remove $app..." "Info"
-    
+
     try {
         # 1. Remove from the current user profile
         Get-AppxPackage -Name $app -AllUsers -ErrorAction SilentlyContinue | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
-        
+
         # 2. Remove the provisioned package so it doesn't install for new users
         $provisioned = Get-AppxProvisionedPackage -Online -ErrorAction SilentlyContinue | Where-Object { $_.DisplayName -like $app }
         if ($provisioned) {
             Remove-AppxProvisionedPackage -Online -PackageName $provisioned.PackageName -ErrorAction SilentlyContinue | Out-Null
         }
-        
+
         $removedCount++
     }
     catch {
